53ba3481d2e59a7cc3ade0dab57feb26b885c448
Multinat <multinat@users.noreply.github.com>
2025-09-04 13:07:00 +0200

    Fix static export issue: Remove unused API route that was preventing static generation

---
+++ out/_next/static/chunks/app/pages/12/page-148acd8d6723b85a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[392,931,459,509,437,417,529,498,525,311,639,755,427,458,13,821,603,346,576,801],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
--- out/_next/static/chunks/app/pages/5/page-0241646c3b3092e4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[821,931,459,509,392,437,417,529,498,525,311,755,639,427,458,13,346,603,576,801],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
